7d3ed3f43defa768c940ade90e83446228c4d75a
Erick Borges <ericklborges@gmail.com>
2019-09-22 00:24:24 -0300

    Update RunAllCircuits to be toggled by a button

--- RunAllCircuits.py
+++ RunAllCircuits.py
@@ -1,13 +1,42 @@
 import subprocess
+from gpiozero import LED, Button
+from signal import pause
 from time import sleep
 
 delay = 0.1
+isRunning = false
 
-subprocess.Popen(["python", "Circuit_10A.py"])
-sleep(delay)
+circuit10AProcess = ""
+circuit20AProcess = ""
+circuit32AProcess = ""
 
-subprocess.Popen(["python", "Circuit_20A.py"])
-sleep(delay)
+def toggle():
+	if isRunning:
+		stop()
+	else:
+		run()
 
-subprocess.Popen(["python", "Circuit_32A.py"])
-sleep(delay)
+def run():
+	isRunning = true
+
+	circuit10AProcess = subprocess.Popen(["python", "Circuit_10A.py"])
+	sleep(delay)
+
+	circuit20AProcess = subprocess.Popen(["python", "Circuit_20A.py"])
+	sleep(delay)
+
+	circuit32AProcess = subprocess.Popen(["python", "Circuit_32A.py"])
+	sleep(delay)
+
+def stop():
+	isRunning = false
+	circuit10AProcess.kill()
+	circuit20AProcess.kill()
+	circuit32AProcess.kill()
+
+led = LED(18)
+button = Button(23)
+
+button.when_pressed = toggle
+
+pause()
